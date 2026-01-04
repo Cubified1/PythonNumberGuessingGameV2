d4bde2fb4f7f9f637262dee75fca855c34e7d119
Cubified <138412300+Cubified1@users.noreply.github.com>
2026-01-04 15:36:44 -0500

    Added Difficulty Levels

--- Main.py
+++ Main.py
@@ -6,25 +6,36 @@ print("welcome to python number guessing game")
 
 input("Press hit enter to continue")
 
-
 def number_guessing():
-    RandomNum = random.randint(1, 100)
-    lowest_number = 1
-    highest_number = 100
+    user_level = input("Please enter your level Easy, Medium or Hard: ").lower()
+    if user_level == "easy":
+        lowest_number = 1
+        highest_number = 50
+    elif user_level == "medium":
+        lowest_number = 1
+        highest_number = 100
+    elif user_level == "hard":
+        lowest_number = 1
+        highest_number = 1000
+    else:
+        print("Invalid input")
     guesses = 0
     is_running = True
+
+    RandomNum = random.randint(lowest_number, highest_number)
+
 
 
     while is_running:
 
-        user_guess = input("Guess your number from 1 to 100: ")
+        user_guess = input(f"Please enter your guess from {lowest_number} to {highest_number}: ")
 
         if user_guess.isdigit():
 
             user_guess = int(user_guess)
             guesses += 1
 
-            if user_guess < 1 or user_guess > 100:
+            if user_guess < lowest_number or user_guess > highest_number:
                 print(f"Please enter a number between {lowest_number} and {highest_number}")
 
             elif user_guess > RandomNum:
@@ -62,6 +73,10 @@ def continue_again():
     else:
         print("Please enter y or n")
         continue_again()
+
+
+
+
 
 
 
