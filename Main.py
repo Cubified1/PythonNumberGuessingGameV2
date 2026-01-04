80d25e29bc464c539cc1414ee590cd03c0d319e1
Cubified <138412300+Cubified1@users.noreply.github.com>
2026-01-03 20:22:22 -0500

    Added where if you guess wrong u it'll say either too low or too high depending on input and the actual answer and made it so if u type something that's not a number it'll reprompt you and I also fixed where the code gives an error if u type a string and also made it where once you get the answer correct it will ask a prompt to ask if u wanna replay

---
+++ Main.py
@@ -0,0 +1,65 @@
+import random
+import time
+
+
+def number_guessing():
+    RandomNum = random.randint(1, 100)
+    lowest_number = 1
+    highest_number = 100
+    guesses = 0
+    is_running = True
+
+
+    while is_running:
+
+        user_guess = input("Guess your number from 1 to 100: ")
+
+        if user_guess.isdigit():
+
+            user_guess = int(user_guess)
+            guesses += 1
+
+            if user_guess < 1 or user_guess > 100:
+                print(f"Please enter a number between {lowest_number} and {highest_number}")
+
+            elif user_guess > RandomNum:
+                print("Your guess is too high")
+
+            elif user_guess < RandomNum:
+                print("Your guess is too low")
+
+            else:
+                print("You guessed the number!")
+                if guesses == 1:
+                    print(f"it took you {guesses} guess")
+                else:
+                    print(f"it took you {guesses} guesses")
+                is_running = False
+        else:
+            print("Invalid input")
+number_guessing()
+
+
+def continue_again():
+    close_counter = 0
+
+
+    again = input("Do you want to continue (y/n)? ")
+    while again == "y":
+        number_guessing()
+        continue_again()
+
+    if again == "n":
+        print("Goodbye!")
+        for close_counter in reversed(range(0, 4)):
+            time.sleep(1)
+            print(f"closing in {close_counter}")
+
+    else:
+        print("Please enter y or n")
+        continue_again()
+
+
+
+
+continue_again()
